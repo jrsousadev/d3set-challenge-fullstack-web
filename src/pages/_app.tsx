9c207a877e8459f342a43437183980d9a270aac3
Junior Sousa <jrsousadev@gmail.com>
2022-09-08 17:54:44 -0300

    (main): configure _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
-import { AppProps } from "next/app"
+import { CssBaseline } from "@mui/material";
+import { AppProps } from "next/app";
+import { GlobalStyle } from "../../styles/global";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "../components/Header";
+import { Content } from "../../styles/pages";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <GlobalStyle />
+      <div style={{ position: "absolute", top: "0", right: "0" }}>
+        <ToastContainer />
+      </div>
+      <Header />
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
